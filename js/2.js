22f61782d5a000ca8d8d81837723a14b6d7e3441
Gretio Rumuat <s21910147@student.unklab.ac.id>
2020-12-09 22:34:15 +0800

    final project

---
+++ js/2.js
@@ -0,0 +1,14 @@
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow");
+});
+
+
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+			  "images/3.jpeg", 
+		
+	 			], 	{duration: 3200, fade: 1300});
+		});
+})
